7c8f41ef7af96f6bac71f34b6047b5835f439625
Akash Goswami <165215439+akashgoswami139@users.noreply.github.com>
2026-08-26 17:02:26 +0530

    [4] [Time Beats: 100.00%] [Memory Beats: 14.90%] - LeetPush

---
+++ 4-Median-of-Two-Sorted-Arrays.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+
+        # Always binary search on the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m = len(nums1)
+        n = len(nums2)
+
+        left = 0
+        right = m
+
+        while left <= right:
+
+            partition1 = (left + right) // 2
+            partition2 = (m + n + 1) // 2 - partition1
+
+            maxLeft1 = float('-inf') if partition1 == 0 else nums1[partition1 - 1]
+            minRight1 = float('inf') if partition1 == m else nums1[partition1]
+
+            maxLeft2 = float('-inf') if partition2 == 0 else nums2[partition2 - 1]
+            minRight2 = float('inf') if partition2 == n else nums2[partition2]
+
+            # Correct partition
+            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
+
+                # Odd number of elements
+                if (m + n) % 2 == 1:
+                    return max(maxLeft1, maxLeft2)
+
+                # Even number of elements
+                return (max(maxLeft1, maxLeft2) +
+                        min(minRight1, minRight2)) / 2
+
+            # Move partition1 to the left
+            elif maxLeft1 > minRight2:
+                right = partition1 - 1
+
+            # Move partition1 to the right
+            else:
+                left = partition1 + 1
