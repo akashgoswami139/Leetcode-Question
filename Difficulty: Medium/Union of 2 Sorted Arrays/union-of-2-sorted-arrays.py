9f51b16eb8a56294c7e304131513598f14e209e9
Akash Goswami <165215439+akashgoswami139@users.noreply.github.com>
2026-09-13 20:21:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.py
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.py
@@ -1,26 +1,35 @@
 class Solution:
     def findUnion(self, a, b):
-        n,m= len(a),len(b)
-        new= []
-        i,j = 0,0
-        while i<n and j<m:
-            if a[i]==b[j]:
-                if not new or new[-1] != a[i]:
-                    new.append(a[i])
-                i+=1
-                j+=1
-            elif a[i]> b[j]:
-                if not new or new[-1] != b[j]:
-                    new.append(b[j])
-                j+=1
-            elif a[i]< b[j]:
-                if not new or new[-1] != a[i]:
-                    new.append(a[i])
-                i+=1
-        for k in range(i, n):
-            if not new or new[-1] != a[k]:
-                new.append(a[k])
-        for k in range(j, m):
-            if not new or new[-1] != b[k]:
-                    new.append(b[k])        
-        return new
+        n, m = len(a), len(b)
+        i, j = 0, 0
+        result = []
+
+        while i < n and j < m:
+
+            if a[i] < b[j]:
+                if not result or result[-1] != a[i]:
+                    result.append(a[i])
+                i += 1
+
+            elif b[j] < a[i]:
+                if not result or result[-1] != b[j]:
+                    result.append(b[j])
+                j += 1
+
+            else:
+                if not result or result[-1] != a[i]:
+                    result.append(a[i])
+                i += 1
+                j += 1
+
+        while i < n:
+            if not result or result[-1] != a[i]:
+                result.append(a[i])
+            i += 1
+
+        while j < m:
+            if not result or result[-1] != b[j]:
+                result.append(b[j])
+            j += 1
+
+        return result
